4668347de66fa2f3e1d79a7fb07fe6a199f19bed
Gabriel De Lima <130858293+GabrielLima1227@users.noreply.github.com>
2024-08-16 14:57:21 -0300

    feat: Finalized Functionality and Styles

--- newbie-faq-accordion-main/main.js
+++ newbie-faq-accordion-main/main.js
@@ -1,3 +1,23 @@
-
-
-
+document.addEventListener('DOMContentLoaded', function() {
+    // Seleciona todos os botões de título de pergunta
+    const buttons = document.querySelectorAll('.faq_content_question_title');
+  
+    buttons.forEach(function(button) {
+      button.addEventListener('click', function() {
+        // Seleciona a resposta e o ícone associados ao botão clicado
+        const icon = this.querySelector('.icon');
+        const response = this.nextElementSibling;
+  
+        // Alterna a visibilidade da resposta e troca o ícone
+        if (response.style.display === 'block' || response.style.display === '') {
+          response.style.display = 'none';
+          icon.src = 'assets/images/icon-plus.svg';
+          icon.alt = 'Expand';
+        } else {
+          response.style.display = 'block';
+          icon.src = 'assets/images/icon-minus.svg';
+          icon.alt = 'Collapse';
+        }
+      });
+    });
+  });
